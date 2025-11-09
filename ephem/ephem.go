21427c85f7f59601b42adf4f0e8be3739d03eac1
ilbagatto <sergei.krushinski@gmail.com>
2025-11-09 14:31:52 +0300

    minor fix

--- ephem/ephem.go
+++ ephem/ephem.go
@@ -114,8 +114,8 @@ func ppLambda(p EclCoord) float64 { return p.Lambda }
 // stepFor picks a numerical step (in days) per body for a stable derivative.
 func stepFor(body Body) float64 {
 	switch body {
-	case Moon:
-		return 1.0 / 720.0 // 2 min
+	// case Moon: // not used
+	// 	return 1.0 / 720.0 // 2 min
 	case Mercury, Venus, Sun, Mars:
 		return 1.0 / 96.0 // 15 min
 	default:
